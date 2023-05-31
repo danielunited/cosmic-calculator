f70628ea02f4c91401eca1120bfa089ed1f4ae55
Daniel Alter <daniel.united@windowslive.com>
2023-05-31 11:10:00 +0300

    Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["@/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   app: {
     buildAssetsDir: "/create/_nuxt",
   },
