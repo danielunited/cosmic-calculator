2198fb706083e35963d89485ec9ddfbe17b545e9
Daniel Alter <daniel.united@windowslive.com>
2023-05-31 22:37:58 +0300

    Events, DOB, copy

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen text-gray-700 bg-gray-50">
     <div class="max-w-lg p-5">
-      <h1 class="mb-4 text-3xl font-light">Universe Age Calculator</h1>
-      <p class="mb-4 text-lg">Enter your date of birth and choose an event to find out when it happened in terms of your lifespan if it were equivalent to the age of the Universe.</p>
+      <h1 class="mb-4 text-3xl font-light">Universe to human years</h1>
+      <p class="mb-4 text-lg">Map cosmic timescales onto your lifespan. Enter your birth date, pick an event, and visualize when it would occur in your universe-age life.</p>
 
       <form @submit.prevent="calculateEvent">
         <div class="w-64">
@@ -52,35 +52,40 @@ const today = new Date();
 const currentYear = today.getFullYear();
 
 const events = [
-  { name: "Creation of the first galaxies", year: -13000000000 },
-  { name: "Formation of the Milky Way", year: -10000000000 },
-  { name: "Formation of the Earth", year: -4500000000 },
-  { name: "Emergence of first life on Earth", year: -3800000000 },
-  { name: "Appearance of the dinosaurs", year: -230000000 },
-  { name: "Extinction of the dinosaurs", year: -66000000 },
-  { name: "Emergence of Sahelanthropus", year: -7000000 },
-  { name: "Emergence of Homo Sapiens", year: -300000 },
+  { name: "Emergence of the first galaxies", year: -13800000000 },
+  { name: "Formation of the Milky Way galaxy", year: -13400000000 },
+  { name: "Formation of the Solar System and the Sun", year: -4600000000 },
+  { name: "Formation of Earth", year: -4530000000 },
+  { name: "Formation of the Moon", year: -4450000000 },
+  { name: "Emergence of the first life on Earth", year: -3500000000 },
+  { name: "Emergence of multicellular life", year: -600000000 },
+  { name: "Appearance of dinosaurs", year: -230000000 },
+  { name: "Appearance of the first mammals", year: -200000000 },
+  { name: "Extinction of dinosaurs", year: -66000000 },
+  { name: "Emergence of the Sahelanthropus species", year: -7000000 },
+  { name: "Emergence of Homo Sapiens species", year: -300000 },
   { name: "Discovery of fire", year: -125000 },
-  { name: "Agricultural revolution", year: -12000 },
-  { name: "Invention of the wheel", year: -5200 },
-  { name: "Founding of the Roman Empire", year: -27 },
+  { name: "Agricultural revolution", year: -10000 },
+  { name: "Invention of the wheel", year: -3500 },
+  { name: "Construction of Pyramids", year: -2560 },
+  { name: "Establishment of the Roman Empire", year: -27 },
   { name: "Birth of Jesus Christ", year: 0 },
   { name: "Fall of the Roman Empire", year: 476 },
+  { name: "Newton's Principia published", year: 1687 },
+  { name: "Invention of the Steam Engine", year: 1712 },
   { name: "Industrial revolution", year: 1760 },
+  { name: "First public flight", year: 1903 },
   { name: "Moon landing", year: 1969 },
-  { name: "Sun becomes a Red Giant", year: 5000000000 },
-  { name: "Death of the Earth", year: 7500000000 },
-  { name: "Milky Way merges with Andromeda", year: 4500000000 },
+  // Future events
+  // { name: "Sun becomes a Red Giant", year: 5000000000 },
+  // { name: "Death of the Earth", year: 7500000000 },
+  // { name: "Milky Way merges with Andromeda", year: 4500000000 },
 ];
 
 function calculateEvent() {
   const dobValue = new Date(dob.value);
-  let age = currentYear - dobValue.getFullYear();
-  const monthDifference = today.getMonth() - dobValue.getMonth();
 
-  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dobValue.getDate())) {
-    age--;
-  }
+  const age = (today - dobValue) / (365.25 * 24 * 60 * 60 * 1000);
 
   const universeAgeInYears = 13.8 * Math.pow(10, 9);
   const eventYear = events.find((event) => event.name === selectedEvent.value).year;
